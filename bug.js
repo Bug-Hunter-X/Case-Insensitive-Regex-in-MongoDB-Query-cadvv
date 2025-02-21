2fec877986a839e51f4478b8ac5088d4c497a631
BugHunter-X <askzenai@gmail.com>
2025-02-21 18:58:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+const query = { name: /John/i }; // Case-insensitive search
+db.collection('users').find(query).toArray((err, result)=>{
+  if(err) throw err;
+  console.log(result); 
+})
+```
